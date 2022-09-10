abb7714c2699343f00a1a49cbcb48732062bcee8
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-10 16:01:28 +0530

    Added: Compute the Quotient & Remainder Logic

--- src/com/blz/QuotientRemainder/QuotientRemainder.java
+++ src/com/blz/QuotientRemainder/QuotientRemainder.java
@@ -1,9 +1,25 @@
 package com.blz.QuotientRemainder;
-
+import java.util.Scanner;
 public class QuotientRemainder {
 
 	public static void main(String[] args) {
 System.out.println("Welcome to Compute Quotient & Remainder Program!!");
+   Scanner scanner = new Scanner(System.in);
+   System.out.println("Enter dividend: ");
+   int dividend = scanner.nextInt();
+   
+   System.out.println("Enter divisor: ");
+   int divisor = scanner.nextInt();
+   
+   int Quotient = dividend/divisor;
+   int Remainder = dividend % divisor;
+   
+   System.out.println("The Quotient is: "+Quotient);
+   System.out.println("The Remainder is: "+Remainder);
+             
+
+
+
 
 	}
 
